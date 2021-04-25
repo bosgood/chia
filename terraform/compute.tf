bafb9d13f637894aa9f0c00d7b8a75eb49b5e902
Brad Osgood <bosgood@gmail.com>
2021-04-25 16:27:17 -0400

    Split TF, destroy EBS volumes

---
+++ terraform/compute.tf
@@ -0,0 +1,34 @@
+resource "aws_key_pair" "laptop" {
+  key_name   = var.ssh_key_name
+  public_key = var.ssh_public_key
+}
+
+resource "aws_instance" "farmer" {
+  # # Fedora CoreOS
+  # # https://getfedora.org/coreos/download?tab=cloud_launchable&stream=stable
+  # ami       = "ami-06af204ec574cc791"
+  # user_data = var.farmer_user_data
+
+  # Amazon Linux 2 HVM
+  # https://aws.amazon.com/amazon-linux-2/faqs/
+  ami = "ami-0742b4e673072066f"
+
+  availability_zone = var.availability_zone
+  instance_type     = "t2.micro"
+  subnet_id         = aws_subnet.chia.id
+  key_name          = aws_key_pair.laptop.key_name
+
+  vpc_security_group_ids = [
+    aws_security_group.allow_ssh.id,
+    aws_security_group.allow_all_outbound.id,
+  ]
+
+  tags = {
+    project = "chia"
+  }
+
+  root_block_device {
+    volume_size = 10
+    volume_type = "gp3"
+  }
+}
---
+++ terraform/network.tf
@@ -0,0 +1,55 @@
+resource "aws_vpc" "chia" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_hostnames = true
+
+  tags = {
+    project = "chia"
+  }
+}
+
+resource "aws_subnet" "chia" {
+  vpc_id                  = aws_vpc.chia.id
+  cidr_block              = "10.0.0.0/24"
+  availability_zone       = var.availability_zone
+  map_public_ip_on_launch = true
+
+  tags = {
+    project = "chia"
+  }
+}
+
+
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow SSH inbound traffic"
+  vpc_id      = aws_vpc.chia.id
+
+  ingress {
+    description = "SSH ingress"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = var.home_ips
+  }
+
+  tags = {
+    project = "chia"
+  }
+}
+
+resource "aws_security_group" "allow_all_outbound" {
+  name        = "allow_all_outbound"
+  description = "Allow all outbound traffic"
+  vpc_id      = aws_vpc.chia.id
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    project = "chia"
+  }
+}
