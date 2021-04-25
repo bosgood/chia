e016f409b25530852fd1a294b6ecb798ef782e93
Brad Osgood <bosgood@gmail.com>
2021-04-25 17:58:38 -0400

    Add FCOS worker version

--- terraform/compute.tf
+++ terraform/compute.tf
@@ -3,12 +3,7 @@ resource "aws_key_pair" "laptop" {
   public_key = var.ssh_public_key
 }
 
-resource "aws_instance" "farmer" {
-  # # Fedora CoreOS
-  # # https://getfedora.org/coreos/download?tab=cloud_launchable&stream=stable
-  # ami       = "ami-06af204ec574cc791"
-  # user_data = var.farmer_user_data
-
+resource "aws_instance" "farmer_amzn" {
   # Amazon Linux 2 HVM
   # https://aws.amazon.com/amazon-linux-2/faqs/
   ami = "ami-0742b4e673072066f"
@@ -32,3 +27,29 @@ resource "aws_instance" "farmer" {
     volume_type = "gp3"
   }
 }
+
+# resource "aws_instance" "farmer_fcos" {
+#   # Fedora CoreOS
+#   # https://getfedora.org/coreos/download?tab=cloud_launchable&stream=stable
+#   ami       = "ami-06af204ec574cc791"
+#   user_data = var.farmer_user_data
+
+#   availability_zone = var.availability_zone
+#   instance_type     = "t2.micro"
+#   subnet_id         = aws_subnet.chia.id
+#   key_name          = aws_key_pair.laptop.key_name
+
+#   vpc_security_group_ids = [
+#     aws_security_group.allow_ssh.id,
+#     aws_security_group.allow_all_outbound.id,
+#   ]
+
+#   tags = {
+#     project = "chia"
+#   }
+
+#   root_block_device {
+#     volume_size = 10
+#     volume_type = "gp3"
+#   }
+# }
